c020cfeff72d4c0e4930f5a818f2343fb87e3521
19kummitha <113335352+19kummitha@users.noreply.github.com>
2025-04-03 10:48:04 +0530

    updated contact details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,12 +7,12 @@ export default function Contact() {
       <ul className="space-y-4">
         <li className="flex items-center text-lg">
           <FaMobileAlt className="mr-3 text-blue-500" />
-          <span>+123 456 7890</span>
+          <span>+91-8499857148</span>
         </li>
         <li className="flex items-center text-lg">
           <FaEnvelope className="mr-3 text-red-500" />
           <a href="mailto:your-email@example.com" className="hover:underline">
-            your-email@example.com
+            venkatakoti18@gmail.com
           </a>
         </li>
         <li className="flex items-center text-lg">
